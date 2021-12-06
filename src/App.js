82e41484b2c1cb1e1a90f972694a925fa140acd3
Utkarsh Hadgekar <79980596+utkarsh-1602@users.noreply.github.com>
2021-12-06 20:54:02 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Creating a React App and Deploying it on Github Pages
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Utkarsh
         </a>
       </header>
     </div>
